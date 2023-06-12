893ab4d5244c594e25358d02ff97ccd97e161d0c
LuisEnrique25 <luisrg2225@gmail.com>
2023-06-12 17:42:30 -0600

    Aplique un aviso cuando no tenemos usuarios registrados

--- src/components/UsersContainer.jsx
+++ src/components/UsersContainer.jsx
@@ -6,10 +6,14 @@ const UsersContainer = ({users,
   changeShowModal, 
   setIsUserToUpdate, 
   setIsLoading}) => {
+
+    console.log(users);
+
   return (
     <section className='grid gap-8 grid-cols-[repeat(auto-fill,_290px)] sm:grid-cols-[repeat(auto-fill,_350px)] justify-center max-w-[1300px] mx-auto px-2 py-6 transition-all duration-500 ease-in'>
         
         {
+           users.length === 0 ? <h3 className='absolute w-fit top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-sm sm:text-lg text-gray-700'>Sin Usuarios Registrados</h3> : 
             users.map((user) => <UserCard key={user.id} user={user} deleteUser={deleteUser} changeShowModal={changeShowModal} setIsUserToUpdate={setIsUserToUpdate} setIsLoading={setIsLoading}/>)
         }
         
